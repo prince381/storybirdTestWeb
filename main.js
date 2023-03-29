2dcc32d18ad8a9fa73347a8daab6c671f22fb26a
prince381 <powusu381@gmail.com>
2023-03-29 19:22:14 +0000

    chore: initial commit for frontend of storybird services test

---
+++ main.js
@@ -0,0 +1,68 @@
+import './style.css'
+import axios from 'axios';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const apiURL = 'http://localhost:5000/api/v1';
+  
+  const imageList = document.querySelector('.image-list');
+  const currentImage = document.getElementById('current');
+  const audio = document.getElementById('audio');
+  const form = document.querySelector('.input-form');
+  const button = document.getElementById('generate');
+
+  form.addEventListener('submit', async (e) => {
+    button.textContent = 'Generating...';
+    e.preventDefault();
+    const input = document.getElementById('storyData');
+    const inputVal = input.value;
+    
+    if (!inputVal) return;
+
+    const story = JSON.parse(inputVal);
+    console.log(story);
+
+    const generateImages = axios.post(`${apiURL}/image`, { story: story });
+    const generateAudio = axios.post(`${apiURL}/audio`, { story: story });
+
+    const [imageResponse, audioResponse] = await Promise.all([generateImages, generateAudio]);
+    console.log(imageResponse, audioResponse);
+
+  });
+
+  const audioEvent = new EventSource(`${apiURL}/audio-stream`);
+  const imageEvent = new EventSource(`${apiURL}/image-stream`);
+
+  audioEvent.addEventListener('message', (e) => {
+    const data = JSON.parse(e.data);
+    if (data.url) {
+      audio.src = data.url;
+      audio.play();
+      button.textContent = 'Submit';
+    };
+    console.log(data);
+  });
+
+  audioEvent.addEventListener('error', (err) => {
+    console.log(err);
+  });
+
+  imageEvent.addEventListener('message', (e) => {
+    const data = JSON.parse(e.data);
+    console.log(data);
+    if (data.imageUrls) {
+      currentImage.src = data.imageUrls[0];
+      currentImage.removeAttribute('hidden');
+      data.imageUrls.forEach((url) => {
+        const imgContainer = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = url;
+        imgContainer.appendChild(img);
+        imageList.appendChild(imgContainer);
+      });
+    }
+  });
+
+  imageEvent.addEventListener('error', (err) => {
+    console.log(err);
+  });
+})
